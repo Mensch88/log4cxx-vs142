74b77856e08dce0a32d19b4f2be621f6fb5e4c86
Krzysztof Bociurko <git@chanibal.pl>
2020-02-12 19:07:25 +0100

    Properly compiling release targets

--- example/example.cpp
+++ example/example.cpp
@@ -2,12 +2,6 @@
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "mswsock.lib")
 #pragma comment(lib, "Rpcrt4.lib")
-#pragma comment(lib, "D:/Projekty/log4cxxWin32/msvc15/lib/x64/Release-static/apr.lib")
-#pragma comment(lib, "D:/Projekty/log4cxxWin32/msvc15/lib/x64/Release-static/aprutil.lib")
-#pragma comment(lib, "D:/Projekty/log4cxxWin32/msvc15/lib/x64/Release-static/log4cxx.lib")
-#pragma comment(lib, "D:/Projekty/log4cxxWin32/msvc15/lib/x64/Release-static/xml.lib")
-
-
 
 // include log4cxx header files.
 #include "log4cxx/logger.h"
@@ -15,46 +9,36 @@
 #include "log4cxx/helpers/exception.h"
 #include "log4cxx/ndc.h"
 
-
 #include <chrono>
 
 int main()
 {
-    //auto rootLogger = log4cxx::Logger::getRootLogger();
-    //rootLogger->getLoggerRepository()->
-
-
+    printf("begin\n");
     log4cxx::LoggerPtr logger(log4cxx::Logger::getLogger("example"));
     LOG4CXX_INFO(logger, "Lorem ipsum sid doloret");
-    LOG4CXX_INFO(logger, "Lorem ipsum sid doloret");
 
-   /* log4cxx::LoggerPtr logger(log4cxx::Logger::getLogger("example"));
-    log4cxx::LoggerPtr logger2(log4cxx::Logger::getLogger("example.asdf"));
-    logger->info("asdf", LOG4CXX_LOCATION);
-    LOG4CXX_INFO(logger, "from macro");
+    using namespace std::chrono;
+    using namespace std::chrono_literals;
 
-    log4cxx::MDC mdc("mdckey", "val");
-    LOG4CXX_INFO(logger, "from macro with MDC?");
-
-    log4cxx::NDC ndc("test");
-    LOG4CXX_INFO(logger, "from macro with NDC?");
-    LOG4CXX_INFO(logger2, "from macro with NDC?/2");
-    LOG4CXX_INFO(logger2, "from macro with NDC?/2");
-
-
-    std::chrono::steady_clock clock;
+    steady_clock clock;
     const auto before = clock.now();
-    const int max = 100;
-    for (int i = 0; i < max; i++)
+    const auto until = before + 1000ms;
+    int iteration = 0;
+    while (clock.now() < until)
     {
-        log4cxx::MDC mdc2("now_ns", std::to_string(clock.now().time_since_epoch().count()));
-        log4cxx::MDC mdc3("iteration", std::to_string(i));
-        LOG4CXX_INFO(logger, "from macro with NDC?");
+        iteration++;
+        log4cxx::MDC mdc("now_ns", std::to_string(clock.now().time_since_epoch().count()));
+        LOG4CXX_DEBUG(logger, "beep #" << iteration);
     }
+    const auto after = clock.now();
+    const auto diff = after - before;
 
-    const auto diff = (clock.now() - before);
-    std::cout << "Logging " << max << " entries took " << diff.count() * 0.000000001 <<"s (" << (diff.count()/max) * 0.000000001 << "s per entry)" << std::endl;
+    using milliseconds_frac = duration<double, std::milli>;
+    using seconds_frac = duration<double>;
 
-    std::cout << "Hello World!\n";*/
+    LOG4CXX_INFO(logger, "Entities per second: " << iteration / duration_cast<seconds_frac>(diff).count());
+    LOG4CXX_INFO(logger, "Miliseconds per entity: " << duration_cast<milliseconds_frac>(diff).count() / iteration);
+
+    printf("end\n");
 }
 
